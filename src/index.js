44f8f2945de66fbbb614840544f363faac906de1
DevEwertonOliveira <ewerton2007@gmail.com>
2023-04-09 14:43:04 -0400

    redux toolkit configurado

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import store from "./store/configureStore";
-import { addTask, removeTask, completedTask, fetchTodo } from "./store/tasks";
+import { addTask, removeTask, completedTask } from "./store/tasks";
+import { addEmployee } from "./store/employess";
 
 const unsubscribe = store.subscribe(() => {
   console.log("Updated", store.getState());
 });
 
-store.dispatch(addTask("Task 1"));
-store.dispatch(addTask("Task 2"));
+store.dispatch(addTask({ task: "Task 1" }));
+store.dispatch(addTask({ task: "Task 2" }));
 console.log(store.getState());
 
-// unsubscribe();
-store.dispatch(completedTask(2));
+unsubscribe();
+store.dispatch(completedTask({ id: 2 }));
 
-store.dispatch(removeTask(1));
-store.dispatch(fetchTodo());
+store.dispatch(removeTask({ id: 1 }));
 console.log(store.getState());
+
+store.dispatch(addEmployee({ name: "Harley" }));
--- src/store/tasks.js
+++ src/store/tasks.js
@@ -1,53 +1,27 @@
-// Action Types
-const ADD_TASK = "ADD_TASK";
-const REMOVE_TASK = "REMOVE_TASK";
-const TASK_COMPLETED = "TASK_COMPLETED";
+import { createSlice } from "@reduxjs/toolkit";
 
-// Actions
-export const addTask = (task) => {
-  return { type: ADD_TASK, payload: { task: task } };
-};
-
-export const removeTask = (id) => {
-  return { type: REMOVE_TASK, payload: { id: id } };
-};
-
-export const completedTask = (id) => {
-  return { type: TASK_COMPLETED, payload: { id: id } };
-};
-
-export const fetchTodo = () => async (dispatch) => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-  const task = await response.json();
-  dispatch(addTask(task.title));
-};
-
-// Reducers
 let id = 0;
+const taskSlice = createSlice({
+  name: "tasks",
+  initialState: [],
+  reducers: {
+    addTask: (state, action) => {
+      state.push({
+        id: ++id,
+        task: action.payload.task,
+        completed: false,
+      });
+    },
+    removeTask: (state, action) => {
+      const index = state.findIndex((task) => task.id === action.payload.id);
+      state.splice(index, 1);
+    },
+    completedTask: (state, action) => {
+      const index = state.findIndex((task) => task.id === action.payload.id);
+      state[index].completed = true;
+    },
+  },
+});
 
-export default function reducer(state = [], action) {
-  switch (action.type) {
-    case ADD_TASK:
-      return [
-        ...state,
-        {
-          id: ++id,
-          task: action.payload.task,
-          completed: false,
-        },
-      ];
-    case REMOVE_TASK:
-      return state.filter((task) => task.id !== action.payload.id);
-    case TASK_COMPLETED:
-      return state.map((task) =>
-        task.id === action.payload.id
-          ? {
-              ...task,
-              completed: true,
-            }
-          : task
-      );
-    default:
-      return state;
-  }
-}
+export const { addTask, removeTask, completedTask } = taskSlice.actions;
+export default taskSlice.reducer;
