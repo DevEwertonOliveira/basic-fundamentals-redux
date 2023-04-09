070705d02f433d16ba2fb578a01cbbb87cba1c6c
DevEwertonOliveira <ewerton2007@gmail.com>
2023-04-09 12:32:35 -0400

    basic fundamentals of redux

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import store from "./store/configureStore";
+import { addTask, removeTask, completedTask, fetchTodo } from "./store/tasks";
+
+const unsubscribe = store.subscribe(() => {
+  console.log("Updated", store.getState());
+});
+
+store.dispatch(addTask("Task 1"));
+store.dispatch(addTask("Task 2"));
+console.log(store.getState());
+
+// unsubscribe();
+store.dispatch(completedTask(2));
+
+store.dispatch(removeTask(1));
+store.dispatch(fetchTodo());
+console.log(store.getState());
