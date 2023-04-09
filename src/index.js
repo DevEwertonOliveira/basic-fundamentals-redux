6d3eb0f5a91c56b7d7930d083ecaa59e507e889b
DevEwertonOliveira <ewerton2007@gmail.com>
2023-04-09 18:20:53 -0400

    estudo de chamada de api pronto

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
 import store from "./store/configureStore";
-import { addTask, removeTask, completedTask } from "./store/tasks";
-import { addEmployee } from "./store/employess";
+import {
+    loadTasks,
+    addNewTask,
+    updateCompleted,
+    deleteTask,
+} from "./store/tasks";
 
-const unsubscribe = store.subscribe(() => {
-  console.log("Updated", store.getState());
-});
+// const gettingTasks = async () => {
+//     try {
+//         // calling api
+//         const response = await axios.get("http://localhost:5000/api/tasks");
+//         console.log(response);
 
-store.dispatch(addTask({ task: "Task 1" }));
-store.dispatch(addTask({ task: "Task 2" }));
-console.log(store.getState());
+//         // dispatch action
+//         store.dispatch(getTasks({ tasks: response.data }));
+//     } catch (error) {
+//         store.dispatch({
+//             type: "SHOW_ERROR",
+//             payload: { error: error.message },
+//         });
+//     }
+// };
 
-unsubscribe();
-store.dispatch(completedTask({ id: 2 }));
+// gettingTasks();
 
-store.dispatch(removeTask({ id: 1 }));
-console.log(store.getState());
-
-store.dispatch(addEmployee({ name: "Harley" }));
-store.dispatch({ type: "SHOW_ERROR", payload: { error: "User not found!" } });
+store.dispatch(loadTasks());
+store.dispatch(addNewTask({ task: "Complete This Exercise" }));
+store.dispatch(updateCompleted({ id: 6, completed: true }));
+store.dispatch(deleteTask({ id: 6 }));
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,19 +1,19 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
-import employeeReducer from "./employess";
+import { configureStore } from "@reduxjs/toolkit";
+import employeeReducer from "./employees";
 import taskReducer from "./tasks";
-import logger from "redux-logger";
-import error from "../middleware/error";
+import error from "./middleware/error";
+import api from "./middleware/api";
 
 const store = configureStore({
-  reducer: {
-    tasks: taskReducer,
-    employees: employeeReducer,
-  },
-  middleware: (getDefaultMiddleware) => [
-    ...getDefaultMiddleware(),
-    logger,
-    error,
-  ],
+    reducer: {
+        tasks: taskReducer,
+        employees: employeeReducer,
+    },
+    middleware: (getDefaultMiddleware) => [
+        ...getDefaultMiddleware(),
+        api,
+        error,
+    ],
 });
 
 export default store;
